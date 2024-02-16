9785ee52f900f345b7d6e13de774b20e2c31d04c
krypton225 <k.mohamed.11.98@gmail.com>
2024-02-16 17:29:51 +0200

    Update `tailwind.config.js` file by adding colors & configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,pug}", "./*{.html}"],
+  content: ["./src/**/*.{html,pug}", "./*.{html}"],
   theme: {
-    extend: {},
+    colors: {
+      "nutmeg": "hsl(14, 45%, 36%)",
+      "dark-raspberry": "hsl(332, 51%, 32%)",
+      "rose-white": "hsl(330, 100%, 98%)",
+      "eggshell": "hsl(30, 54%, 90%)",
+      "light-grey": "hsl(30, 18%, 87%)",
+      "wenge-brown": "hsl(30, 10%, 34%)",
+      "dark-charcoal": "hsl(24, 5%, 18%)",
+    },
+    extend: {
+      fontFamily: {
+        "outfit": ["Outfit", "sans-serif"],
+        "young": ["Young Serif", "sans-serif"]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    (process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  ],
 };
